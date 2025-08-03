9358cb97c6fd2dfb38cf1b5daacf5958c0f76600
Elias <elias.duda21@gmail.com>
2025-08-03 15:22:38 +0200

    volume sliders

---
+++ Assets/Scripts/AdiioVolumeSlider.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+public class AdiioVolumeSlider : MonoBehaviour
+{
+    public Slider slider;
+    public bool isSFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (isSFX)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.getParameterByName("GlobalSFXVolume", out float vol);
+            slider.value = vol;
+        }
+        else
+        {
+            FMODUnity.RuntimeManager.StudioSystem.getParameterByName("GlobalMusicVolume", out float vol);
+            slider.value = vol;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetVolumeOnSliderChanged(float volume)
+    {
+        if (isSFX)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("GlobalSFXVolume", volume);
+        }
+        else
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("GlobalMusicVolume", volume);
+        }
+    }
+}
