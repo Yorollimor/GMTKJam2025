21842ef3ec3a1232d7300ec12a4aa9016cc5f5b7
Yorollimor <53179730+Yorollimor@users.noreply.github.com>
2025-08-06 00:34:41 +0200

    Rings despawning in blue and red tank fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,15 +29,6 @@ public class GameManager : MonoBehaviour
             Destroy(this); // Destroy duplicate instances
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
 
 
     public void LoadMainScene()
